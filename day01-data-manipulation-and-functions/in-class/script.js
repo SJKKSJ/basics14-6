dacee8c166425e5f7cb03f4ef1ab943cbb4e1bd3
SJKKSJ <101933596+SJKKSJ@users.noreply.github.com>
2022-03-26 22:52:24 +0800

    In Class Day 1 Exercises Part 2 completed all Base to More Comfortable problems

--- day01-data-manipulation-and-functions/in-class/script.js
+++ day01-data-manipulation-and-functions/in-class/script.js
@@ -1,16 +1,27 @@
+// Day 1 Base Fahrenheit to Celsius
+
+// Enter Fahrenheit then convert to Celsius
+// Celcius = (Fahrenheit-32) x 5/9
+var fahrenheitToCelsiusMainHelper = function (fahrenheit) {
+  var celsius = ((fahrenheit - 32) * 5) / 9;
+  return celsius;
+};
+
 var fahrenheitToCelsiusMain = function (input) {
   // Complete the Base: Fahrenheit to Celsius exercise below with fahrenheitToCelsiusMain as the main function.
-  var myOutputValue = "hello world";
+  var myOutputValue = fahrenheitToCelsiusMainHelper(input);
   return myOutputValue;
 };
 
 // Day 1 Base Road Trip Cost
 
+// Converting KM into Miles
 var convertKmToMiles = function (distanceInKm) {
   var distanceInMiles = distanceInKm * 0.62;
   return distanceInMiles;
 };
 
+// Calculating the Ferrari fuel cost according to the trip miles
 var calculateTotalFuelCost = function (tripLengthInMiles) {
   // Some code that calculates total fuel cost
   var fuelCost = (tripLengthInMiles / 9) * 2.2;
@@ -19,17 +30,41 @@ var calculateTotalFuelCost = function (tripLengthInMiles) {
 
 var roadTripCostBaseMain = function (input) {
   // Complete the Base: Road Trip Cost exercise below with roadTripCostBaseMain as the main function.
+  // Calling the formula for converting the input trip length to miles
   var distanceInMiles = convertKmToMiles(input);
+  // Calling the fuel cost formula to calculate the road trip cost
   var myOutputValue = calculateTotalFuelCost(distanceInMiles);
   return myOutputValue;
 };
 
 // Day 1 Comfortable Road Trip Cost
 
+// Converting KM into Miles
+var convertKmToMiles = function (distanceInKm) {
+  var distanceInMiles = distanceInKm * 0.62;
+  return distanceInMiles;
+};
+
+// Calculating fuel cost accd to the trip miles
+var calculateTotalFuelCostForComfortable = function (
+  tripLengthInMiles,
+  costPerLitreOfFuel
+) {
+  // Some code that calculates total fuel cost
+  var fuelCost = (tripLengthInMiles / 9) * costPerLitreOfFuel;
+  return fuelCost;
+};
+
+// Calculating Trip Savings
 var roadTripCostComfortableMain = function (input) {
   // Complete the Comfortable: Road Trip Cost exercise below with roadTripCostComfortableMain as the main function.
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  var distanceInMiles = convertKmToMiles(input);
+  // difference in the fuel price for Ferrai and Train is 0.2
+  var savingsInFuel = calculateTotalFuelCostForComfortable(
+    distanceInMiles,
+    0.2
+  );
+  return savingsInFuel;
 };
 
 // Day 1 More Comfortable Ice Machine
@@ -60,16 +95,52 @@ var iceMachineMain = function (input) {
   return totalHours;
 };
 
-//
+// Day 1 More Comfortable Beer Order
+
+// avg no. of customers per day - 2 pints per visit
+// half barrel sized kegs needed per quarter
+// half barrel keg equivalent to 124 pints of beer
+
+// total pints of beer in a quarter = no. of customer * 2 * daysInQtr
+var calculateTotalPints = function (customers) {
+  // how many weeks in a qtr, then how many days in a qtr
+  var daysInQtr = (52 / 4) * 7;
+  var totalPintsPerQtr = customers * 2 * daysInQtr;
+  return totalPintsPerQtr;
+};
+
+// total half barrel kegs needed per quarter = total pints of beer in a qtr / 124
+var convertPintsToKegs = function (totalPintsPerQtr) {
+  var totalKegsPerQtr = totalPintsPerQtr / 124;
+  return totalKegsPerQtr;
+};
 
 var beerOrderMain = function (input) {
   // Complete the More Comfortable: Beer Order exercise below with beerOrderMain as the main function.
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  var totalPintsPerQtr = calculateTotalPints(input);
+  var totalKegsPerQtr = convertPintsToKegs(totalPintsPerQtr);
+  return totalKegsPerQtr;
+};
+
+// Day 1 More Comfortable Cost of Cellular Data
+
+// 19.99 for 50GB
+// Data GB entered -> how much paid?
+// if exceed 50 GB, auto addt 50GB purchased
+// Math.ceil() rounds a number up to nearest integer
+
+// from the no. of data used per month
+var calculateCostOfDataUsedPerMonth = function (noOfGBUsedPM) {
+  // calculate the no. of plans purchased
+  var noOfDataPlans = Math.ceil(noOfGBUsedPM / 50);
+  // total cost of data plan paid for the month
+  var totalDataPlanBill = noOfDataPlans * 19.99;
+  return totalDataPlanBill;
 };
 
 var cellularDataMain = function (input) {
   // Complete the More Comfortable: Cost of Cellular Data exercise below with cellularDataMain as the main function.
-  var myOutputValue = "hello world";
-  return myOutputValue;
+  // find the avg cost per GB, from the total cost paid per month divide by amount of GB used
+  var averageCostPerGB = calculateCostOfDataUsedPerMonth(input) / input;
+  return averageCostPerGB;
 };
