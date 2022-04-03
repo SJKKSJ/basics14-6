7dc9a9f7d3097e337b51d3ecd2562f4da5452cbe
SJKKSJ <101933596+SJKKSJ@users.noreply.github.com>
2022-04-03 15:18:22 +0800

    SPS Part 1 (base) + Part 2 (win loss record) updated the whole block of codes

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -34,41 +34,41 @@ var main = function (input) {
   // Player puts out scissors, if computer plays paper, user wins, else lose
   else if (playerOption == "scissors") {
     if (computerOption == "paper") {
-      myOutputValue = "You win";
+      myOutputValue = "You win! ";
       userWinRecord = userWinRecord + 1;
     } else {
-      myOutputValue = "You lose";
+      myOutputValue = "You lose! ";
       userLoseRecord = userLoseRecord + 1;
     }
   }
   // Player puts out paper, if computer plays stone, user wins, else lose
   else if (playerOption == "paper") {
     if (computerOption == "stone") {
-      myOutputValue = "You win";
+      myOutputValue = "You win! ";
       userWinRecord = userWinRecord + 1;
     } else {
-      myOutputValue = "You lose";
+      myOutputValue = "You lose! ";
       userLoseRecord = userLoseRecord + 1;
     }
   }
   // Player puts out stone, if computer plays scissors, user wins, else lose
   else if (playerOption == "stone") {
     if (computerOption == "scissors") {
-      myOutputValue = "You win";
+      myOutputValue = "You win! ";
       userWinRecord = userWinRecord + 1;
     } else {
-      myOutputValue = "You lose";
+      myOutputValue = "You lose! ";
       userLoseRecord = userLoseRecord + 1;
     }
-    // total number of games played, to calculate win loss percentage
-    var totalGamesPlayed = userWinRecord + userLoseRecord + userDrawRecord;
-
-    return `${myOutputValue} Wins: ${userWinRecord}(${
-      (userWinRecord * 100) / totalGamesPlayed
-    }%) Losses: ${userLoseRecord}(${
-      (userLoseRecord * 100) / totalGamesPlayed
-    }%) Draws: ${userDrawRecord}(${
-      (userDrawRecord * 100) / totalGamesPlayed
-    }%) Total Games Played: ${userWinRecord + userLoseRecord + userDrawRecord}`;
   }
+  // total number of games played, to calculate win loss percentage
+  var totalGamesPlayed = userWinRecord + userLoseRecord + userDrawRecord;
+
+  return `${myOutputValue} Wins: ${userWinRecord}(${
+    (userWinRecord * 100) / totalGamesPlayed
+  }%) Losses: ${userLoseRecord}(${
+    (userLoseRecord * 100) / totalGamesPlayed
+  }%) Draws: ${userDrawRecord}(${
+    (userDrawRecord * 100) / totalGamesPlayed
+  }%) Total Games Played: ${userWinRecord + userLoseRecord + userDrawRecord}`;
 };
