bbb273349f95e184007a1d72b4f19b6e3724b992
SJKKSJ <101933596+SJKKSJ@users.noreply.github.com>
2022-04-03 15:15:25 +0800

    SPS Part 2 (win loss) added in the counter for wins/losses/draws/total games played + win loss percentage

--- project1-sps/script.js
+++ project1-sps/script.js
@@ -1,4 +1,8 @@
-// Part 1 Base Submission
+// record user's Win Loss Draw score
+var userWinRecord = 0;
+var userLoseRecord = 0;
+var userDrawRecord = 0;
+
 // computerOption helper function
 var computerSPS = function (input) {
   var randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -25,30 +29,46 @@ var main = function (input) {
   // if player and computer same option, draw
   if (playerOption == computerOption) {
     myOutputValue = "It's a draw.";
+    userDrawRecord = userDrawRecord + 1;
   }
   // Player puts out scissors, if computer plays paper, user wins, else lose
   else if (playerOption == "scissors") {
     if (computerOption == "paper") {
       myOutputValue = "You win";
+      userWinRecord = userWinRecord + 1;
     } else {
       myOutputValue = "You lose";
+      userLoseRecord = userLoseRecord + 1;
     }
   }
   // Player puts out paper, if computer plays stone, user wins, else lose
   else if (playerOption == "paper") {
     if (computerOption == "stone") {
       myOutputValue = "You win";
+      userWinRecord = userWinRecord + 1;
     } else {
       myOutputValue = "You lose";
+      userLoseRecord = userLoseRecord + 1;
     }
   }
   // Player puts out stone, if computer plays scissors, user wins, else lose
   else if (playerOption == "stone") {
     if (computerOption == "scissors") {
       myOutputValue = "You win";
+      userWinRecord = userWinRecord + 1;
     } else {
       myOutputValue = "You lose";
+      userLoseRecord = userLoseRecord + 1;
     }
+    // total number of games played, to calculate win loss percentage
+    var totalGamesPlayed = userWinRecord + userLoseRecord + userDrawRecord;
+
+    return `${myOutputValue} Wins: ${userWinRecord}(${
+      (userWinRecord * 100) / totalGamesPlayed
+    }%) Losses: ${userLoseRecord}(${
+      (userLoseRecord * 100) / totalGamesPlayed
+    }%) Draws: ${userDrawRecord}(${
+      (userDrawRecord * 100) / totalGamesPlayed
+    }%) Total Games Played: ${userWinRecord + userLoseRecord + userDrawRecord}`;
   }
-  return myOutputValue;
 };
